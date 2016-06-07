e1edcb5f6c0bbc0ca3a5db328c4362d570ce0b10
Luis Osnaya <osnet@outlook.com>
2016-06-07 13:24:31 -0500

    Se elimina responsiveSlides y se crea slider propio.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
-$(function() {
-    $(".rslides").responsiveSlides();
-  });
+// $(function() {
+//     $(".rslides").responsiveSlides();
+//   });
 
 
 /*	Toggle navigation button.	*/
@@ -18,15 +18,82 @@ $(function () {
     });
 });
 
-// You can also use "$(window).load(function() {"
-    $(function () {
-      // Slideshow 2
-      $("#slider2").responsiveSlides({
-        auto: true,             // Boolean: Animate automatically, true or false
-        speed: 500,            // Integer: Speed of the transition, in milliseconds
-        timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
-        pager: false,
-        manualControls: ""
-      });
+// // You can also use "$(window).load(function() {"
+//     $(function () {
+//       // Slideshow 2
+//       $("#slider2").responsiveSlides({
+//         auto: true,             // Boolean: Animate automatically, true or false
+//         speed: 500,            // Integer: Speed of the transition, in milliseconds
+//         timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+//         pager: false,
+//         manualControls: ""
+//       });
 
+//     });
+
+
+/*  It allows the transition of photos of the main gallery (slider.)  */
+$(document).ready(function() {
+    var time = setInterval(function(){SliderNextImage();}, 8000);
+    $('#btnBack').click(function() {
+        var size = $('.Slider-container').find('.Slider-image').size();
+        $('.Slider-container').find('.Slider-image').each(function (index,value){
+            if ($(this).hasClass('Slider-visible')) {
+                $(this).slideUp();
+                $(this).removeClass('Slider-visible');
+                if (index == 0) {
+                    $($('.Slider-container').find('.Slider-image').get(size - 1)).fadeIn();
+                    $($('.Slider-container').find('.Slider-image').get(size - 1)).addClass('Slider-visible');
+
+                    return false;
+                }   else {
+                    $($('.Slider-container').find('.Slider-image').get(index - 1)).fadeIn();
+                    $($('.Slider-container').find('.Slider-image').get(index - 1)).addClass('Slider-visible');
+                    return false;
+                }
+            };
+        });
     });
+    $('#btnNext').click(function() {
+        var size = $('.Slider-container').find('.Slider-image').size();
+        $('.Slider-container').find('.Slider-image').each(function (index,value){
+            if ($(this).hasClass('Slider-visible')) {
+                $(this).slideUp();
+                $(this).removeClass('Slider-visible');
+                if (index + 1 < size) {
+                    $($('.Slider-container').find('.Slider-image').get(index + 1)).fadeIn();
+                    $($('.Slider-container').find('.Slider-image').get(index + 1)).addClass('Slider-visible');
+                    return false;
+                }   else {
+                    $($('.Slider-container').find('.Slider-image').get(0)).fadeIn();
+                    $($('.Slider-container').find('.Slider-image').get(0)).addClass('Slider-visible');
+                    return false;
+                }
+            };
+        });
+    });
+});
+function SliderNextImage () {
+    var size = $('.Slider-container').find('.Slider-image').size();
+    $('.Slider-container').find('.Slider-image').each(function (index, value) {
+        if ($(this).hasClass('Slider-visible')) {
+            $(this).slideUp();
+            $(this).removeClass('Slider-visible');
+
+            /*  Animacion de imagenes segundarias   */
+            $('.bounceInUp').addClass('animated bounceInUp');
+            $('.bounceInDown').addClass('animated bounceInDown');
+
+            if (index + 1 < size) {
+                $($('.Slider-container').find('.Slider-image').get(index + 1)).fadeIn();
+                $($('.Slider-container').find('.Slider-image').get(index + 1)).addClass('Slider-visible');
+                return false;
+            }   else {
+                $($('.Slider-container').find('.Slider-image').get(0)).fadeIn();
+                $($('.Slider-container').find('.Slider-image').get(0)).addClass('Slider-visible');
+                return false;
+            };
+        };
+    });
+};
+
