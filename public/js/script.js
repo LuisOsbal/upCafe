1baa61d1b101caa69e77f4869ffa716ca5c8df17
Luis Osnaya <osnet@outlook.com>
2016-06-06 19:13:33 -0500

    Estructura del slider con ResponsiveSlides.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,8 @@
+$(function() {
+    $(".rslides").responsiveSlides();
+  });
+
+
 /*	Toggle navigation button.	*/
 $('button').on('click', function() {
 	$(this).toggleClass('isActive');
@@ -12,3 +17,16 @@ $(function () {
         menu.toggleClass('isActiveMenu');
     });
 });
+
+// You can also use "$(window).load(function() {"
+    $(function () {
+      // Slideshow 2
+      $("#slider2").responsiveSlides({
+        auto: true,             // Boolean: Animate automatically, true or false
+        speed: 500,            // Integer: Speed of the transition, in milliseconds
+        timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+        pager: false,
+        manualControls: ""
+      });
+
+    });
